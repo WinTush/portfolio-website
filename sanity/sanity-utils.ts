08ea16f8219fd16d92f3697aa8b0aac30435605b
Winston Kinoti <prog.kinoti@gmail.com>
2023-04-30 22:24:56 +0300

    feat: Add getProject function to retrieve a specific project
    This commit adds the getProject function to retrieve a specific project
     by slug using the Sanity client.

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -20,3 +20,24 @@ export async function getProjects(): Promise<Project[]> {
 }`
   )
 }
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    projectId: 'jun6ipwm',
+    dataset: 'production',
+    apiVersion: '2023-04-29',
+  })
+
+  return client.fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`,
+    { slug }
+  )
+}
